242c237a4b4bded83a478021a297734f1ab46995
GeekEfrat <97566468+EfratGeek@users.noreply.github.com>
2022-10-24 22:30:51 +0300

    documentation

--- test2.py
+++ test2.py
@@ -1,6 +1,11 @@
 import requests
 import base64
 
+# in index.js: var API_URL = 'https://api.demoblaze.com';
+# type: 'POST',
+# url: API_URL + '/login',
+# data: JSON.stringify({"username": username, "password": pass})
+
 s = '123456789'
 EncodedString = base64.b64encode(bytes(s, 'utf-8')).decode('utf-8')
 # the url to get the token to login
@@ -8,6 +13,12 @@ res1 = requests.post(url='https://api.demoblaze.com/login', json={"username": 'E
                      verify=False)
 j1 = res1.json()
 tok = str(j1).split(': ')[1]
+
+# in cart.js: var API_URL = 'https://api.demoblaze.com';
+# type: 'POST',
+# url: API_URL + '/viewcart',
+# data: JSON.stringify({"cookie": token, "flag": true})
+
 
 # login to user, and get the product in the cart
 res = requests.post(url='https://api.demoblaze.com/viewcart',
@@ -18,6 +29,11 @@ json_ = res.json()
 if len(json_["Items"]) != 1:
     raise Exception
 id_ = json_["Items"][0]["prod_id"]
+
+# var API_URL = 'https://api.demoblaze.com';
+# type: 'POST',
+# url: API_URL + '/view',
+# data: JSON.stringify({"id": articleItem.prod_id})
 
 # send the id to see all details of product
 res = requests.post(url='https://api.demoblaze.com/view', json={"id": id_},
